eb31e893bbe61b9641dff3efef2f38b57a0ad425
Ngome Denovine Ingutia <ngomedenovine@gmail.com>
2025-08-24 06:04:31 +0300

    Initial Commit

---
+++ week1.py
@@ -0,0 +1,41 @@
+number = 10
+site_name = "power learn Project"
+print (number)
+print (site_name)
+
+site_name = "I love coding in Python"
+print (site_name)
+
+site_name = "I will be concetrating from today henceforth for me to build a career in programming"
+print (site_name)
+
+a,b,c = 10, 20, 30
+print(a)
+print(b)
+print(c)
+
+#Python Data Types
+num1 = 10
+num2 = 20.5
+print(num1)
+print(num2)
+
+#python List
+languages = ["Python", "Java", "C++", "JavaScript"]
+print(languages)
+print(languages[0])  # Accessing the first element
+
+#Python dictiobnary data type
+capital_cities = {"kenya": "Nairobi", "Tanzania": "Dodoma", "Uganda": "Kampala"}
+print(capital_cities)
+print(capital_cities["kenya"]) # Accessing a value by key
+print(capital_cities["Tanzania"]) # Accessing another value by key  
+print(capital_cities["Uganda"]) # Accessing another value by keys#
+
+while True:
+    name = input("What is your name? ")
+    color = input ("What is your favorite color? ")
+    print(f"Hello {name}, your favourite coloer is {color}.")
+    again = input("Do you want to greet another person? (yes/no) ")
+    if again.lower() != "yes":
+        break
